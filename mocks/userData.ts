d20143c917719d63099d1a23cd45ab0f054ffc1d
Kim Do Young <difbfl4750@gmail.com>
2025-02-12 18:05:57 +0900

    feat: RootLayout & Page 설정, Tailwind CSS Custom Font 설정, MockData 생성

---
+++ mocks/userData.ts
@@ -0,0 +1,5 @@
+export const mockUsers = [
+  { id: '1', name: 'User 1', avatar: '/Profile1.png' },
+  { id: '2', name: 'User 2', avatar: '/Profile2.png' },
+  { id: '3', name: 'User 3', avatar: '/Profile3.png' },
+];
